af9c6886107e78a7c0fb92eba7661e351f129a0d
Maxime HERMOUET <maxime.hermouet@niji.fr>
2014-04-03 22:13:39 +0000

    add activity tracking depending on tasks

--- app/controllers/activity.js
+++ app/controllers/activity.js
@@ -1,29 +1,49 @@
 var forms = require('forms'),
     fields = forms.fields,
+    widgets = forms.widgets,
     validators = forms.validators,
     customValidators = require('../forms/validators'),
-    Activity = require('../db/sql').Activity;
+    Activity = require('../db/sql').Activity,
+    Task = require('../db/sql').Task;
 
-function getNewActivityForm() {
-    return forms.create({
-        date: fields.date({
-            required: true,
-            errorAfterField: true,
-            // validators: [customValidators.dateFr],
-            cssClasses: {
-                label: ['control-label'],
-                field: ['form-group']
-            }
-        }),
-        duration: fields.string({
-            required: true,
-            errorAfterField: true,
-            // validators: [customValidators.duration],
-            cssClasses: {
-                label: ['control-label'],
-                field: ['form-group']
-            }
-        })
+function getNewActivityForm(callback) {
+    Task.findAll().done(function(error, tasks) {
+        var result = {};
+        if (!error) {
+            tasks.forEach(function(task) {
+                result[task.code] = task.description;
+            })
+            var form = forms.create({
+                date: fields.date({
+                    required: true,
+                    errorAfterField: true,
+                    cssClasses: {
+                        label: ['control-label'],
+                        field: ['form-group']
+                    }
+                }),
+                duration: fields.string({
+                    required: true,
+                    errorAfterField: true,
+                    // validators: [customValidators.duration],
+                    cssClasses: {
+                        label: ['control-label'],
+                        field: ['form-group']
+                    }
+                }),
+                task: fields.string({
+                    required: true,
+                    choices: result,
+                    widget: widgets.select(),
+                    errorAfterField: true,
+                    cssClasses: {
+                        label: ['control-label'],
+                        field: ['form-group']
+                    }
+                })
+            });
+            callback(form);
+        }
     });
 }
 
@@ -31,61 +51,76 @@ exports.indexAction = function(req, res) {
     Activity.findAll({
         where: {
             UserId: req.user.uuid
-        }
+        },
+        include: [Task]
     }).success(function(activities) {
-        res.render('activity/index', {
-            title: 'Activities',
-            nav_class: 'navbar-tasks',
-            user: req.user,
-            form: getNewActivityForm().toHTML(),
-            activities: activities
+        getNewActivityForm(function(form) {
+            res.render('activity/index', {
+                title: 'Activities',
+                nav_class: 'navbar-tasks',
+                user: req.user,
+                form: form.toHTML(),
+                activities: activities
+            });
         });
     });
 }
 
 exports.activityCreateAction = function(req, res) {
-    var form = getNewActivityForm();
-    console.log('posted')
-    form.handle(req, {
-        success: function(form) {
-            // there is a request and the form is valid
-            // form.data contains the submitted data
-            Activity.create({
-                date: form.data.date,
-                duration: form.data.duration // TODO: format duration in minutes or forms : formatter ?
-            })
-                .done(function(error, activity) {
-                    if (error) {
-                        console.log(error)
-                        req.flash('error', "couldn't save activity.");
-                    } else {
-                        activity.setUser(req.user);
-                        req.flash('success', 'Activity added.');
+    getNewActivityForm(function(form) {
+        form.handle(req, {
+            success: function(form) {
+                // there is a request and the form is valid
+                // form.data contains the submitted data
+                Activity.create({
+                    date: form.data.date,
+                    duration: form.data.duration
+                })
+                    .done(function(error, activity) {
+                        if (error) {
+                            req.flash('error', "couldn't save activity.");
+                            res.redirect('/admin/activities');
+                        } else {
+                            activity.setUser(req.user);
+                            Task.find({
+                                where: {
+                                    code: form.data.task
+                                }
+                            }).done(function(error, task) {
+                                if (task) {
+                                    activity.setTask(task).success(function() {
+                                        req.flash('success', 'Activity added.');
+                                        res.redirect('/admin/activities');
+                                    });
+                                } else {
+                                    req.flash('success', 'Activity added but no Task linked.');
+                                    res.redirect('/admin/activities');
+                                }
+                            });
+                        }
+                    });
+            },
+            error: function(form) {
+                // the data in the request didn't validate,
+                // calling form.toHTML() again will render the error messages
+                Activity.findAll({
+                    where: {
+                        UserId: req.user.uuid
                     }
-                    res.redirect('/admin/activities');
+                }).success(function(activities) {
+                    res.render('activity/index', {
+                        title: 'Activities',
+                        nav_class: 'navbar-tasks',
+                        user: req.user,
+                        form: form.toHTML(),
+                        activities: activities
+                    });
                 });
-        },
-        error: function(form) {
-            // the data in the request didn't validate,
-            // calling form.toHTML() again will render the error messages
-            Activity.findAll({
-                where: {
-                    UserId: req.user.uuid
-                }
-            }).success(function(activities) {
-                console.log('error2')
-                res.render('activity/index', {
-                    title: 'Activities',
-                    nav_class: 'navbar-tasks',
-                    user: req.user,
-                    form: form.toHTML(),
-                    activities: activities
-                });
-            });
-        },
-        empty: function(form) {
-            exports.indexAction(req, res);
-        }
+            },
+            empty: function(form) {
+                exports.indexAction(req, res);
+            }
+        });
     });
 }
 
